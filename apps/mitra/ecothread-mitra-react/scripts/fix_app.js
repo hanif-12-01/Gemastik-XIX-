18a560e4a4ec739c1049637cddb569fbc23deb79
Auto User <auto@example.com>
2026-07-27 18:16:01 +0700

    chore(mitra): remove nested legacy mitra folder

--- apps/mitra/ecothread-mitra-react/scripts/fix_app.js
+++
@@ -1,1 +0,0 @@
-const fs = require('fs'); let f = 'D:/LOMBA/GEMASTIK/ecothread-mitra-react/src/App.jsx'; let c = fs.readFileSync(f, 'utf8'); c = c.replace(/\\\/g, '\').replace(/\\\$/g, '$'); fs.writeFileSync(f, c);
